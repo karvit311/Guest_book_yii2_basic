9638bb7e142824cefdb2ddb9809397574f180d6c
Tsumanchuk Liubava <karvit311@gmail.com>
2018-06-11 13:13:10 +0300

    set mailer in config/web

--- config/web.php
+++ config/web.php
@@ -37,10 +37,18 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'viewPath' => '@app/mailer',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'karvit311',
+                'password' => 'romanovazinaida311',
+                'port' => '587',
+
+                'encryption' => 'tls',
+            ],
+            'useFileTransport' => false,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
