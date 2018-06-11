a2de9ca4a9f8bf0c8a792a16f197c595d4771f13
Tsumanchuk Liubava <karvit311@gmail.com>
2018-06-11 13:25:42 +0300

    add Active Form in home page for leave comments

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,47 @@
-<?php
+<?php 
+use app\models\Comments;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+$model = new Comments();
+$request  = Yii::$app->request; 
+$session = Yii::$app->session;
+$user_session = $session->get('user_id');
+$comments = $model->getAll();
 ?>
-<div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Comments PAGE</title>
+    <meta charset="utf-8">
+</head>
+    <body>
+        <div id="comments-header">
+            <h1>Comments Page</h1>
         </div>
+        <div id="comments-form">
+            <h3>Please add your comment</h3>
+            <label>Comments:</label>
+            <?php 
+            $form = ActiveForm::begin(); ?>
 
-    </div>
-</div>
+                <?= $form->field($model, 'comment')->textInput(['maxlength' => true])->label(false) ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Submit', ['name' => 'comments']); ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div id="comments-panel">
+            <?php foreach ($comments as $key => $comment) :?>
+                <p <?php if ($comment->user_id == $user_session) 
+                    echo 'style="font-weight:bold;"'?>>
+                    <?php 
+                    echo $comment->comment;?> <span class="comment-date">(<?php echo $comment->created_at.')';?></span>
+                </p>
+            <?php endforeach;?>
+        </div>
+    </body>
+</html>
